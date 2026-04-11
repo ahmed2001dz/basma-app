a65c0836d470f28fd31acb9ba603ad55499959b3
POH <pohdan8@gmail.com>
2026-04-11 21:48:20 +0100

    fix1

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Models\Leson;
+
 use Illuminate\Support\Facades\Route;
 
-$data = Leson::all();
-Route::get('/', function () use ($data) {
-    return view('welcome',compact('data'));
+
+Route::get('/', function () {
+    return view('welcome');
 });
+
