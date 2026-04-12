c0ed20cc92322c956131f5d590352c4fd9187395
POH <pohdan8@gmail.com>
2026-04-12 01:56:09 +0100

    vlast

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,8 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser; // [1] استيراد الواجهة
+use Filament\Panel; // [2] استيراد الـ Panel
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
@@ -11,20 +12,21 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser // [3] تنفيذ الواجهة
 {
     use HasFactory, Notifiable, HasRoles;
-    
-
 
     /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
+     * هذه الدالة هي "المفتاح" الذي يفتح قفل الـ 403 في السحاب
      */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // بما أنك ربطت حسابك برتبة super_admin في TablePlus، فهذا السطر سيجعلك تدخل فوراً
+        return $this->hasRole('super_admin');
+    }
+
     protected function casts(): array
     {
         return [
@@ -33,3 +35,40 @@ class User extends Authenticatable
         ];
     }
 }
+
+
+
+
+// namespace App\Models;
+
+// // use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Database\Factories\UserFactory;
+// use Illuminate\Database\Eloquent\Attributes\Fillable;
+// use Illuminate\Database\Eloquent\Attributes\Hidden;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Notifications\Notifiable;
+// use Spatie\Permission\Traits\HasRoles;
+
+
+// #[Fillable(['name', 'email', 'password'])]
+// #[Hidden(['password', 'remember_token'])]
+// class User extends Authenticatable
+// {
+//     use HasFactory, Notifiable, HasRoles;
+
+
+
+//     /**
+//      * Get the attributes that should be cast.
+//      *
+//      * @return array<string, string>
+//      */
+//     protected function casts(): array
+//     {
+//         return [
+//             'email_verified_at' => 'datetime',
+//             'password' => 'hashed',
+//         ];
+//     }
+// }
